e616c262b02422f0c250d2524198a29a9957a0d5
venum <umarhon167@gm.com>
2025-09-06 18:38:06 +0500

    js changed

--- script.js
+++ script.js
@@ -17,90 +17,46 @@ if (age > 0 && age <= 18) {
 
 // clock start
 
-var clock = +prompt('Введите время');
-switch (clock) {
-    case 1:
-        alert('1 час ночи');
-        break;
-    case 2:
-        alert('2 часа ночи');
-        break;
-    case 3:
-        alert('3 часа ночи');
-        break;
-    case 4:
-        alert('4 часа ночи');
-        break;
-    case 5:
-        alert('5 часов ночи');
-        break;
-    case 6:
-        alert('6 часов утра');
-        break;
-    case 7:
-        alert('7 часов утра');
-        break;
-    case 8:
-        alert('8 часов утра');
-        break;
-    case 9:
-        alert('9 часов утра');
-        break;
-    case 10:
-        alert('10 часов утра');
-        break;
-    case 11:
-        alert('11 часов утра');
-        break;
-    case 12:
-        alert('12 часов утра');
-        break;
-    case 13:
-        alert('1 час дня');
-        break;
-    case 14:
-        alert('2 часа дня');
-        break;
-    case 15:
-        alert('3 часа дня');
-        break;
-    case 16:
-        alert('4 часа дня');
-        break;
-    case 17:
-        alert('5 часов дня');
-        break;
-    case 18:
-        alert('6 часов вечера');
-        break;
-    case 19:
-        alert('7 часов вечера');
-        break;
-    case 20:
-        alert('8 часов вечера');
-        break;
-    case 21:
-        alert('9 часов вечера');
-        break;
-    case 22:
-        alert('10 часов вечера');
-        break;
-    case 23:
-        alert('11 часов вечера');
-        break;
-    case 24:
-        alert('12 часов вечера');
-        break;
-}
+let clock = +prompt("Введите свое время");
 
+if (clock == 0) {
+    alert(clock + 12 + " часов ночи");
+} else if (clock == 1) {
+    alert(clock + " час ночи");
+} else if (clock == 2) {
+    alert(clock + " часа ночи");
+} else if (clock >= 3 && clock <= 4) {
+    alert(clock + " часа ночи");
+} else if (clock >= 5 && clock <= 11) {
+    alert(clock + " часов ночи");
+} else if (clock == 12) {
+    alert(clock + " часов дня");
+} else if (clock == 13) {
+    alert((clock - 12) + " час дня");
+} else if (clock >= 14 && clock <= 16) {
+    alert((clock - 12) + " часа дня");
+} else if (clock >= 17 && clock <= 18) {
+    alert((clock - 12) + " часов дня");
+} else if (clock >= 19 && clock <= 23) {
+    alert((clock - 12) + " часов вечера");
+}
+else {
+    alert('Такого времени нету')
+}
 // clock end
 
 // numbers start
+var num1 = +prompt("Введите первое число:");
+var num2 = +prompt("Введите второе число:");
+var num3 = +prompt("Введите третье число:");
 
-var X = +prompt("Введите первое число")
-var Y = +prompt("Введите второе число")
-var Z = +prompt("Введите третье число")
-var res = (X + Y + Z) / 3;
-alert("Ваше среднее арифметическое значение чисел цифра " + res)
+// Находим среднее число
+if ((num1 >= num2 && num1 <= num3) || (num1 <= num2 && num1 >= num3)) {
+    alert("Среднее число: " + num1);
+} else if ((num2 >= num1 && num2 <= num3) || (num2 <= num1 && num2 >= num3)) {
+    alert("Среднее число: " + num2);
+} else {
+    alert("Среднее число: " + num3);
+}
 
 // numbers end
